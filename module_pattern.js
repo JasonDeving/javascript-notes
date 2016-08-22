5a1ef86c69973f3e3b8267d54b9c4e3fa257953e
jason chan <aznj50@hotmail.com>
2016-08-21 18:12:31 -0700

    module pattern with method chaining

--- module_pattern.js
+++ module_pattern.js
@@ -1,12 +1,23 @@
-var ray = (function(){
-	
+var ray = (function() {
+    var DEFAULTS = {
+        say: 'hello',
+        speed: 'normal'
+    }
 
-	return {
-		speak: function() {
-			var myArg = arguments[0] || '';
-			console.log(myArg.say);
-		}
-	}
+    return {
+        speak: function() {
+            var myArguments = arguments[0] || '';
+            var statement = myArguments.say || DEFAULTS.say;
+            console.log(statement);
+            return this;
+        },
+        run : function() {
+            var myArguments = arguments[0] || '';
+            var running = myArguments.speed || DEFAULTS.speed;
+            console.log('running...'+ running);
+            return this;
+        }
+    };
 })();
 
-ray.speak({ say: 'howyd' });
+ray.speak({ say: 'howdy' }).run().speak({ say: 'run faster' }).run({speed: 'faster'});
