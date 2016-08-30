3635785eab9362be9411e82e90eb694c111a0e7a
jason chan <aznj50@hotmail.com>
2016-08-29 23:42:45 -0700

    add everything

---
+++ collecting_readyline.js
@@ -0,0 +1,17 @@
+var readline = require("readline");
+var rl = readline.createInterface(process.stdin, process.stdout);
+var realPerson = {
+	name: '',
+	sayings: []
+
+}
+
+rl.question("what is the name of a real person " , function(answer){
+	realPerson.name = answer;
+	rl.setPrompt(`What would ${realPerson.name} say `)
+	rl.prompt();
+
+	rl.on('line', function(saying){
+		console.log(saying.trim())
+	});
+})
